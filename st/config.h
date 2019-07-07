597c6ccb45864b5bf2e21e4f363416106a8f7bc8
f-person <agakaryan.arshak@gmail.com>
2019-07-07 13:39:59 +0400

    Apply scrollback patch and add old shortcuts

--- st/config.h
+++ st/config.h
@@ -201,22 +201,63 @@ static MouseShortcut mshortcuts[] = {
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
+#define ALTCONTROL (Mod1Mask|ControlMask)
+
+//static Shortcut shortcuts[] = {
+	//[> mask                 keysym          function        argument <]
+	//{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
+	//{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
+	//{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
+	//{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
+	//{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
+	//{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
+	//{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
+	//{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
+	//{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
+	//{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
+	//{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
+	//{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	//{ MODKEY|ShiftMask,     XK_Prior,       zoom,           {.f = +1} },
+	//{ MODKEY|ShiftMask,     XK_Next,        zoom,           {.f = -1} },
+	//{ MODKEY|ShiftMask,     XK_Home,        zoomreset,      {.f =  0} },
+//};
 
 static Shortcut shortcuts[] = {
-	/* mask                 keysym          function        argument */
-	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
-	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
-	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
-	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
-	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
-	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
-	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
-	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
-	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
-	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+    /* mask                 keysym          function        argument */
+    { XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
+    { ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
+    { ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
+    { XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
+    { TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
+    { TERMMOD,              XK_Next,        zoom,           {.f = -1} },
+    { MODKEY,               XK_Home,        zoomreset,      {.f =  0} },
+    { ShiftMask,            XK_Insert,      clippaste,      {.i =  0} },
+    { MODKEY,               XK_c,           clipcopy,       {.i =  0} },
+    { MODKEY,               XK_v,           clippaste,      {.i =  0} },
+    { MODKEY,               XK_p,           selpaste,       {.i =  0} },
+    { MODKEY,               XK_Num_Lock,    numlock,        {.i =  0} },
+    //{ MODKEY,               XK_Control_L,   iso14755,       {.i =  0} },
+    { ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+    { ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+    { MODKEY,               XK_Page_Up,     kscrollup,      {.i = -1} },
+    { MODKEY,               XK_Page_Down,   kscrolldown,    {.i = -1} },
+    { ALTCONTROL,               XK_k,           kscrollup,      {.i =  1} },
+    { ALTCONTROL,               XK_j,           kscrolldown,    {.i =  1} },
+    { ALTCONTROL,               XK_Up,          kscrollup,      {.i =  1} },
+    { ALTCONTROL,               XK_Down,        kscrolldown,    {.i =  1} },
+    { ALTCONTROL,          XK_u,           kscrollup,      {.i = -1} },
+    { ALTCONTROL,          XK_d,           kscrolldown,    {.i = -1} },
+    { TERMMOD,              XK_Up,          zoom,           {.f = +1} },
+    { TERMMOD,              XK_Down,        zoom,           {.f = -1} },
+    { TERMMOD,              XK_K,           zoom,           {.f = +1} },
+    { TERMMOD,              XK_J,           zoom,           {.f = -1} },
+    { TERMMOD,              XK_U,           zoom,           {.f = +2} },
+    { TERMMOD,              XK_D,           zoom,           {.f = -2} },
+    //{ MODKEY,               XK_l,           externalpipe,   {.v = openurlcmd } },
+    //{ MODKEY,               XK_y,           externalpipe,   {.v = copyurlcmd } },
+    //{ MODKEY,               XK_o,           externalpipe,   {.v = copyoutput } },
 };
+
 
 /*
  * Special keys (change & recompile st.info accordingly)
