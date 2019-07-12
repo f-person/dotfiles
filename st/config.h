f3dda5ddccb87c684f2b17ec07fde8cee4a19ad1
f-person <agakaryan.arshak@gmail.com>
2019-07-13 01:24:22 +0400

    Change padding

--- st/config.h
+++ st/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
-static int borderpx = 1;
+static int borderpx = 5;
 
 /*
  * What program is execed by st depends of these precedence rules:
