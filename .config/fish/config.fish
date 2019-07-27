7feee4edb7939b23e0b57c4a2abe8961cc4d4b75
f-person <agakaryan.arshak@gmail.com>
2019-07-28 01:03:56 +0400

    Add fish configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,19 @@
+export PATH=/bin/lscript:/bin/lscript:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/usr/share/games:/usr/local/sbin:/usr/sbin:/sbin:/home/fperson/local/bin
+
+export PATH=/bin/lscript:/bin/lscript:/bin/lscript:/bin/lscript:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/usr/share/games:/usr/local/sbin:/usr/sbin:/sbin:/home/fperson/local/bin:/home/fperson/bin:/usr/local/go/bin:/home/fperson/go/bin
+
+alias ls=lsd
+alias diadon='python3.6 $HOME/.diadon/diadon.py'
+alias դիադոն='diadon'
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+alias vim=nvim
+
+export PATH="$PATH:/usr/lib/dart/bin"
+export PATH="$PATH:/home/fperson/flutter/bin"
+export PATH="$PATH:/home/fperson/Android/Sdk/tools"
+export PATH="$PATH:/home/fperson/Android/Sdk/platform-tools"
+export PATH="$PATH:/home/fperson/Android/Sdk/build-tools"
+export PATH="$PATH":"$HOME/.pub-cache/bin"
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
+thefuck --alias | source
