940dcddb148f92f06dea919abc6bacf8e44206bc
f-person <agakaryan.arshak@gmail.com>
2019-09-05 23:10:32 +0400

    Use alt+shift for changing fontsize

--- st/config.h
+++ st/config.h
@@ -200,27 +200,8 @@ static MouseShortcut mshortcuts[] = {
 
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
-#define TERMMOD (ControlMask|ShiftMask)
+#define TERMMOD (Mod1Mask|ShiftMask)
 #define ALTCONTROL (Mod1Mask|ControlMask)
-
-//static Shortcut shortcuts[] = {
-	//[> mask                 keysym          function        argument <]
-	//{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
-	//{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
-	//{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
-	//{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	//{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	//{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
-	//{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
-	//{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
-	//{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
-	//{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
-	//{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
-	//{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	//{ MODKEY|ShiftMask,     XK_Prior,       zoom,           {.f = +1} },
-	//{ MODKEY|ShiftMask,     XK_Next,        zoom,           {.f = -1} },
-	//{ MODKEY|ShiftMask,     XK_Home,        zoomreset,      {.f =  0} },
-//};
 
 static Shortcut shortcuts[] = {
     /* mask                 keysym          function        argument */
