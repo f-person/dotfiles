239704ce65eb2f8907a26a1d25de7349e6aae950
f-person <agakaryan.arshak@gmail.com>
2019-12-26 00:19:18 +0400

    Unapply actualfullscreen patch

--- dwm/config.h
+++ dwm/config.h
@@ -95,7 +95,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_w,      setlayout,      {.v = &layouts[5]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
