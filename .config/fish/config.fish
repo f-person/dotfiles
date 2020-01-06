a0b8e325bf441b200aa92be590573724e9f8fa43
f-person <agakaryan.arshak@gmail.com>
2020-01-07 01:04:06 +0400

    Use abbreviations isntead of aliases for ls, vim, please

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,8 @@
 #!/usr/bin/fish
 set -U fish_user_paths /bin/lscript /bin/lscript /bin/lscript /bin/lscript /usr/local/bin /usr/bin /bin /usr/local/games /usr/games /usr/share/games /usr/local/sbin /usr/sbin /sbin /home/fperson/local/bin /home/fperson/bin /usr/local/go/bin /home/fperson/go/bin /usr/lib/dart/bin /home/fperson/.pub-cache/bin /home/fperson/dev/flutter/bin /home/fperson/Android/Sdk/tools /home/fperson/Android/Sdk/platform-tools /home/fperson/Android/Sdk/build-tools /home/fperson/dev/node-v12.11.0-linux-x64/bin /home/fperson/.yarn/bin /home/fperson/.config/yarn/global/node_modules/.bin 
 
-alias ls=lsd
-alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
-alias vim=nvim
-alias please=sudo
+alias config='git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+
+abbr ls lsd
+abbr vim nvim
+abbr please sudo
