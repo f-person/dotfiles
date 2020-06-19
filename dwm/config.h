9b79e38d98e6a7e0a47c01af607c25d4409992a8
f-person <agakaryan.arshak@gmail.com>
2020-06-19 20:02:10 +0400

    apply centeredmaster patch

--- dwm/config.h
+++ dwm/config.h
@@ -55,6 +55,8 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },
 	{ "HHH",      grid },
 	{ "[\\]",     dwindle },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
 };
 
 /* key definitions */
@@ -96,6 +98,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_w,      setlayout,      {.v = &layouts[5]} },
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[6]} },
+	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[7]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
