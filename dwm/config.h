f130a70a23599862233c6803f0f85079c0d6c34d
f-person <agakaryan.arshak@gmail.com>
2020-11-04 03:11:26 +0400

    add kitty config

--- dwm/config.h
+++ dwm/config.h
@@ -73,7 +73,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dmenucmd[] = { "rofi", "-show", "run", NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 #include "push.c"
 static Key keys[] = {
